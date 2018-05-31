e38aec19a45a5672f50eac46e65eaa7dea3619eb
James Nicholson <nicholsonjb@vcu.edu>
2018-05-31 00:13:27 -0400

    Exercise 2 Even Or Odd

---
+++ exercise2evenodd.py
@@ -0,0 +1,15 @@
+#Author: James Nicholson
+#Date: 5/30/2018
+#Ask the user for a number. 
+# Depending on whether the number is even or odd, 
+# print out an appropriate message to the user.
+
+num = int(input("Enter a number: "))
+
+mod = num % 2
+
+if mod > 0:
+    print("Number is odd.")
+else:
+    print("Number is even") 
+3
