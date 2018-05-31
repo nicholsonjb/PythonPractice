eb9d673cbd0908995889a9f1dc9343648485817b
James Nicholson <nicholsonjb@vcu.edu>
2018-05-31 01:08:23 -0400

    Exericse 3 Completed

---
+++ exercise3listlesstanten.py
@@ -0,0 +1,18 @@
+ #Author: James Nicholson
+#Date: 5/30/2018
+#Take a list, say for example this one:
+#a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#and write a program that prints out all the 
+# elements of the list that are less than 5.
+ 
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+num = int(input("Enter a number: "))
+
+new_list = []
+
+for i in a:
+    if i < num:
+        new_list.append(i)
+print(new_list)
