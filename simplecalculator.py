97d0b7b31804624f7d2e20896f5ce6755cf2db64
James Nicholson <nicholsonjb@vcu.edu>
2018-06-01 18:11:06 -0400

    Calculator

---
+++ simplecalculator.py
@@ -0,0 +1,42 @@
+#Author: James Nicholson
+#Date: 6/1/2018
+#Write a program that creates a simple calculator
+
+while True:
+    print("Options: ")
+    print("Enter 'add' to add two numbers")
+    print("Enter 'subtract' to subtract two numbers")
+    print("Enter 'multiply' to multiply two numbers")
+    print("Enter 'divide' to divide two numbers")
+    print("Enter 'quit' to end the program")
+    
+    user_input = input(": ")
+    
+    if user_input == "quit":
+        print("Good-Bye!")
+        break   
+    elif user_input == "add":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        result = str(num1 + num2)
+        print("The answer is "+ result)
+   
+    elif user_input == "subtract":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        result = str(num1 - num2)
+        print("The answer is "+ result)
+    
+    elif user_input == "multiply":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        result = str(num1 * num2)
+        print("The answer is "+ result)
+
+    elif user_input == "divide":
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        result = str(num1 / num2)
+        print("The answer is "+ result)
+    else:
+        print("Invalid Input")
