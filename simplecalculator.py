6d0bd6103a2916e0b2da9409f7ed3b6b06aab95b
nicholsonjb <nicholsonjb@vcu.edu>
2018-06-02 03:05:34 -0400

    Calculator Update

--- simplecalculator.py
+++ simplecalculator.py
@@ -16,26 +16,26 @@ while True:
         print("Good-Bye!")
         break   
     elif user_input == "add":
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
         result = str(num1 + num2)
         print("The answer is "+ result)
    
     elif user_input == "subtract":
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
         result = str(num1 - num2)
         print("The answer is "+ result)
     
     elif user_input == "multiply":
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
         result = str(num1 * num2)
         print("The answer is "+ result)
 
     elif user_input == "divide":
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
         result = str(num1 / num2)
         print("The answer is "+ result)
     else:
