a10c9530857bd48998547f5d2c523a48a047cc5b
nicholsonjb <nicholsonjb@vcu.edu>
2018-06-11 15:39:16 -0400

    Exersice 7 Purpose Update

--- exercise7listcomprehensions.py
+++ exercise7listcomprehensions.py
@@ -1,6 +1,6 @@
  #Author: James Nicholson
 #Date: 6/10/2018
-#Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
+#Write a program that creates a list of 5 to 15 numbers from 1 to 75.
 #Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
 
 import random
