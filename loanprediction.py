576fdea203e74e443833f4131da2680361d1eff5
James Nicholson <nicholsonjb@vcu.edu>
2019-02-10 01:47:33 -0500

    Update

---
+++ loanprediction.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib as plt 
+
+
+df = pd.read_csv("/Users/James Nicholson PC/Google Drive/Data Science Projects/Load Prediction Dataset/train.csv") #Reading the dataset in dataframe  using Panads
+df.head(10)
